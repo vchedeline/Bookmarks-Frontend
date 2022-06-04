357a763ad7e1b89671f2d1b72a21b4ec77cf11e2
Omari Archer <omari.archer21@gmail.com>
2022-06-04 17:26:53 -0400

    CSS update

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import "./index.sass"
 export default function Index({ bookmark, createBookmark }) {
   const [newForm, setNewForm] = useState(null);
 
@@ -16,24 +16,28 @@ export default function Index({ bookmark, createBookmark }) {
     });
   };
 
-  return (
+  return (<>
+
+      <h2>Bookmark your favorite sites</h2>
     <div className="display">
-      <form onSubmit={handleSubmit}>
-        <input
+      <form className="form" onSubmit={handleSubmit}>
+      <h1>Bookmarks</h1>
+        <input className="title"
           type="text"
           name="website"
           value={bookmark.title}
           onChange={handleChange}
           placeholder="website"
         />
-        <input
+        <input className="url"
           type="text"
           name="url"
           value={bookmark.url}
           placeholder="http://"
         />
-        <button type="submit">ADD BOOKMARK</button>
+        <button className="button" type="submit">ADD BOOKMARK</button>
       </form>
     </div>
+    </>
   );
 }
